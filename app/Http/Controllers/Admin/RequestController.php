0f274bbaa0673ee31c6b8d1aa3d8addbd6d091d7
hiep1998vnhn11 <hiep1998vnhn11@gmail.com>
2020-12-04 16:55:53 +0700

    request and order

--- app/Http/Controllers/Admin/RequestController.php
+++ app/Http/Controllers/Admin/RequestController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\AppBaseController;
 use App\Models\Request as ModelsRequest;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
 
 class RequestController extends AppBaseController
 {
+    public $viewerRole, $publicanRole, $adminRole;
     public function __construct()
     {
         $this->middleware(['auth:api', 'role:admin']);
+        $this->viewerRole = Role::find(1);
+        $this->adminRole = Role::find(2);
+        $this->publicanRole = Role::find(4);
     }
 
     public function index()
@@ -21,14 +26,21 @@ class RequestController extends AppBaseController
         return $this->sendRespondSuccess($requests, 'Get request successfully!');
     }
 
-    public function acceptRequest(ModelsRequest $request)
+    public function accept(ModelsRequest $request)
     {
+        if ($request->name === 'Publican') {
+            $user = $request->user;
+            if ($user->hasRole($this->viewerRole)) {
+                $user->removeRole($this->viewerRole);
+                $user->assignRole($this->publicanRole);
+            } else return $this->sendRespondError($user, 'User is an publican', 500);
+        }
         $request->status = 'accepted';
         $request->save();
         return $this->sendRespondSuccess($request, 'Accept successfully!');
     }
 
-    public function cancelRequest(ModelsRequest $request)
+    public function cancel(ModelsRequest $request)
     {
         $request->status = 'canceled';
         $request->save();
--- app/Http/Controllers/Publican/OrderController.php
+++ app/Http/Controllers/Publican/OrderController.php
@@ -7,5 +7,13 @@ use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware(['auth:api', 'role:publican']);
+    }
+
+    public function index()
+    {
+        auth()->user()->pubs;
+    }
 }
